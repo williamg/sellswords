1d85461107420b70962d93443cff121e4fbb7232
William <william.ganucheau@gmail.com>
2014-10-10 18:52:56 -0400

    Made server.js parenthesis spacing consistent

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ var Logger = require(__dirname + "/../shared/logger");
 
 function Server(port) {
 	this._express = express();
-	this._root = path.resolve (__dirname + "/../../");
+	this._root = path.resolve(__dirname + "/../../");
 	this._port = port;
 	this._server = undefined;
 
@@ -15,7 +15,7 @@ function Server(port) {
 	});
 }
 
-Server.prototype.start = function () {
+Server.prototype.start = function() {
 	var s = this;
 	this._server = this._express.listen(this._port, function() {
 		Logger.log(Logger.INFO, "Listening on port " + s._port);
